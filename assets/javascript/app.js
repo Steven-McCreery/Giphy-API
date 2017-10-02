33340daadf8324d516935f743d3bb7e50763e4a3
Steven McCreery <logad8@gmail.com>
2017-10-02 17:57:10 -0400

    created basic files and html structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+
+
+
+var topics = [
+	{topic1: "topic",
+	subtopic1: "subtopic",
+	subtopic2: {sub21 : "subsub1",
+		sub22: "subsub2"},
+	},
+	{topic2: "topic2",
+	subtopic1: "subtopic",
+	subtopic2: {sub21 : {subsubsub1: "subsub1",
+				subsubsub2: "subsub2"},
+		sub22: "subsub2"},
+	},
+]
+
+console.log(topics);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
