826e8e38c2ec547a342d5c69eae467b7fb5efd29
Steven McCreery <logad8@gmail.com>
2017-10-07 03:38:30 -0400

    changed delegated listener and function structure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,49 @@
+$(document).ready(function() {
 
+	drawButtons();
 
-$("#button").on("click", function(){
-	event.preventDefault();
-	newButton();
-})
+	$("#button").on("click", function(){
+		event.preventDefault();
+		newButton();
+	})
 
-$(".buttons").on("click", function(){
-	event.preventDefault();
-	getImages();
-})
-
-$("img").on("click", function(){
-	event.preventDefault();
-	animateToggle();
-})
+	$(".buttons").on("click", "img", function(){
+		event.preventDefault();
+		animateToggle();
+	})
 
 
 
-var tvShows = ["I'ts Always Sunny in Philadelphia", "Simpsons", "30 Rock", "Family Guy", "House of Cards", "Better Off Ted", "Monty Python", "The Blacklist"];
+	var tvShows = ["I'ts Always Sunny in Philadelphia", "Simpsons", "30 Rock", "Family Guy", "House of Cards", "Better Off Ted", "Monty Python", "The Blacklist"];
 
-console.log(tvShows);
-
-
-// console.log(topics);
-
-newButton = function() {
-
-	$("#textToSearch").val().trim();
+	console.log(tvShows);
 
 
-	for (var i = 0; i < currentTopic.length; i++) {
+	// console.log(topics);
 
-		var b = $("<button>");
-		b.text($("#textToSearch").val().trim());
-		b.attr("alt", "clickable image of " + currentTopic[i]);
-		$(".images").append(currentTopic[i]);
-		
+	newButton = function() {
+
+		$tvShows.push($("#textToSearch").val().trim());
+
+		$("#textToSearch").val("");
+
+		drawButtons();
+
+		console.log(tvShows);
 	}
 
-}
+	drawButtons = function() {
+		
+		for (var i = 0; i < currentTopic.length; i++) {
+
+			var b = $("<button>");
+			b.text($("#textToSearch").val().trim());
+			b.attr("alt", "clickable image of " + currentTopic[i]);
+			$(".images").append(currentTopic[i]);
+			
+		}
+	}
 
 
-
-
-console.log($.now());
+	console.log($.now());
+})
