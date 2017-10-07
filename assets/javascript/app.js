b268c63225e1ad4805c62a8b1683ca73f14040d0
Steven McCreery <logad8@gmail.com>
2017-10-07 04:01:43 -0400

    rendering and adding of buttons to array and screen fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,38 @@
 $(document).ready(function() {
+	
 
-	drawButtons();
-
+	// call new button function that also calls button drawing function
 	$("#button").on("click", function(){
 		event.preventDefault();
 		newButton();
 	})
 
+
 	$(".buttons").on("click", "img", function(){
+		event.preventDefault();
+		getImages();
+	})
+
+	// whenever the buttons are drawn, including new elements (not present at document load) they can be listened for to toggle
+	$(".images").on("click", "img", function(){
 		event.preventDefault();
 		animateToggle();
 	})
 
-
-
+	// array for image getting and button drawing
 	var tvShows = ["I'ts Always Sunny in Philadelphia", "Simpsons", "30 Rock", "Family Guy", "House of Cards", "Better Off Ted", "Monty Python", "The Blacklist"];
 
 	console.log(tvShows);
 
-
-	// console.log(topics);
-
 	newButton = function() {
 
-		$tvShows.push($("#textToSearch").val().trim());
+		// add user's cleaned-up input to array
+		tvShows.push($("#textToSearch").val().trim());
 
+		// clear input section
 		$("#textToSearch").val("");
 
+		// call button drawing function
 		drawButtons();
 
 		console.log(tvShows);
@@ -34,14 +40,29 @@ $(document).ready(function() {
 
 	drawButtons = function() {
 		
-		for (var i = 0; i < currentTopic.length; i++) {
+		// clear button area before replacing buttons
+		$(".buttons").empty();
 
+		// looping through array to draw buttons
+		for (var i = 0; i < tvShows.length; i++) {
+
+			// creates blank jQuery button then adds text to button 
 			var b = $("<button>");
-			b.text($("#textToSearch").val().trim());
-			b.attr("alt", "clickable image of " + currentTopic[i]);
-			$(".images").append(currentTopic[i]);
-			
+			b.text(tvShows[i]);	
+			b.addClass("tvShow");		
+			$(".buttons").append(b);
 		}
+	}
+
+	// draw buttons when page loads
+	drawButtons();
+
+	getImages = function() {
+
+		// set URL for Giphly site
+		var queryURL;
+
+
 	}
 
 
